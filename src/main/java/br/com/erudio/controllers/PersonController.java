fff36520db27d0fa9cec6e7adb1c5995cea71894
robertotics4 <robertooliveiragp@gmail.com>
2022-08-25 10:59:21 -0300

    finish

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-
     @Autowired
     private PersonServices service;
 
@@ -23,15 +23,12 @@ public class PersonController {
         return service.findById(id);
     }
 
-    @RequestMapping(
-            method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
     }
 
-    @RequestMapping(
-            method = RequestMethod.POST,
+    @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -39,8 +36,7 @@ public class PersonController {
         return service.create(person);
     }
 
-    @RequestMapping(
-            method = RequestMethod.PUT,
+    @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -48,11 +44,12 @@ public class PersonController {
         return service.update(person);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.DELETE
+    @DeleteMapping(
+            value = "/{id}"
     )
-    public void delete(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
