cf47547333a4e0cd8a50e45b3674912516f84f27
robertotics4 <robertooliveiragp@gmail.com>
2022-08-29 20:04:19 -0300

    config flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (first_name, last_name, address, gender)
+VALUES ('Luis', 'Roberto', 'Turu, São Luis - MA', 'Male'),
+       ('John', 'Doe', 'OROA, New York', 'Male'),
+       ('Doe', 'John', 'ASSD, OKAWA', 'Male');
