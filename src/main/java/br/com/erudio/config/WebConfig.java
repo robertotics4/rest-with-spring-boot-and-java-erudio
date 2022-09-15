0cd88137966302205ba9ede216bf2c19090b02e6
robertotics4 <robertooliveiragp@gmail.com>
2022-09-15 19:28:48 -0300

    feat ContentNegotiation via header

--- src/main/java/br/com/erudio/config/WebConfig.java
+++ src/main/java/br/com/erudio/config/WebConfig.java
@@ -9,10 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer
-                .favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        // VIA QUERY PARAM
+//        configurer
+//                .favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+        // VIA HEADER
+                configurer
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
